b5bcc348a695ae492f0134a8a9b7b273e3b2a045
Riley <riley@e926.de>
2022-07-17 23:05:26 +0200

    added basic_init plugin to fully initialize client

---
+++ basic_init/init.go
@@ -0,0 +1,37 @@
+package main
+
+// This is only a placeholder
+
+import (
+	"github.com/anon55555/mt"
+	"github.com/ev2-1/minetest-go"
+)
+
+func InitHook(c *minetest.Client) {
+	c.SendCmd(&mt.ToCltPrivs{})
+	c.SendCmd(&mt.ToCltDetachedInv{})
+	c.SendCmd(&mt.ToCltMovement{
+		DefaultAccel: 2.4,
+		AirAccel:     1.2,
+		FastAccel:    10,
+		WalkSpeed:    4.317,
+		CrouchSpeed:  1.295,
+		FastSpeed:    30,
+		ClimbSpeed:   2.35,
+		JumpSpeed:    6.6,
+		Fluidity:     1.13,
+		Smoothing:    0.5,
+		Sink:         23,
+		Gravity:      10.4,
+	})
+	c.SendCmd(&mt.ToCltInvFormspec{
+		Formspec: "List main 1\nWidth 5\nEmpty\nEmpty\nEmpty\nEmpty\nEmpty\nEndInventory\n",
+	})
+	c.SendCmd(&mt.ToCltHP{
+		HP: 20,
+	})
+	c.SendCmd(&mt.ToCltBreath{
+		Breath: 10,
+	})
+
+}
